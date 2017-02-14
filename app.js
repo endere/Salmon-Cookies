d42a5d6c34f9adc17795f3643aca314b90eb1e84
Erik Enderlein <erik.end@gmail.com>
2017-02-14 17:15:52 -0600

    made the entire table generation happen inside the javascript.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 'use strict';
-
 function createElement(tagType, tagIdentifier, tagIdentifiername, elementContent, sectionId){
   var element = document.createElement(tagType);
   element.setAttribute(tagIdentifier, tagIdentifiername);
   element.textContent = elementContent;
-  console.log(element);
+//  console.log(element);
   sectionId.appendChild(element);
   //this element creation function created by Benjamin Ayzenberg.
 }
@@ -26,28 +25,64 @@ CookieStore.prototype.generate = function (minCust, maxCust, average, array, sec
     // var text = i + ':00: ' + cookies + ' cookies were sold.';
     createElement('td', 'class', 'cookie-sales', cookies, sectionEl);
   }
-  console.log(array);
   createElement('td', 'class', 'total-sales', total, sectionEl);
 };
 
-function CookieStore(minCust, maxCust, average, sectionEl){
+function CookieStore(minCust, maxCust, average, name, tag){
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.average = average;
   this.array = [];
   this.total = 0;
-  this.sectionEl = sectionEl;
+  this.name = name;
+  this.tag = tag;
 }
 
+/*
+function timeTotal(stores){
+  for (i = 0; i < 17; i++){
+    var total = firstAndPike.array[i] + seaTac.array[i] + seattleCenter.array[i] + capHill.array[i] + alki.array[i];
+    createElement('td', 'class', 'total-sales', total, document.getElementById('totals'));
+  }
+}
+*/
+var firstAndPike = new CookieStore(23, 65, 6.3, 'First and Pike', 'firstAndPike');
+var seaTac = new CookieStore(3, 24, 1.2, 'Sea Tac', 'seaTac');
+var seattleCenter = new CookieStore(11, 38, 3.7, ' Seattle Center', 'seattleCenter');
+var capHill = new CookieStore(20, 38, 2.3, 'Capital Hill', 'capHill');
+var alki = new CookieStore(2, 16, 4.6, 'Alki', 'alki');
 
-var firstAndPike = new CookieStore(23, 65, 6.3, document.getElementById('firstAndPike'));
-var seaTac = new CookieStore(3, 24, 1.2, document.getElementById('seaTac'));
-var seattleCenter = new CookieStore(11, 38, 3.7, document.getElementById('seattleCenter'));
-var capHill = new CookieStore(20, 38, 2.3, document.getElementById('capHill'));
-var alki = new CookieStore(2, 16, 4.6, document.getElementById('alki'));
+var stores = [firstAndPike, seaTac, seattleCenter, capHill, alki];
+var tableEl = document.createElement('table');
+tableEl.setAttribute('id', 'tableEl');
+var head = document.createElement('thead');
+tableEl.appendChild(head);
+head.setAttribute('id', 'head');
+var times = ['Location', '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', 'Total'];
 
-firstAndPike.generate(firstAndPike.minCust, firstAndPike.maxCust, firstAndPike.average, firstAndPike.array, firstAndPike.sectionEl, firstAndPike.total);
-seaTac.generate(seaTac.minCust, seaTac.maxCust, seaTac.average, seaTac.array, seaTac.sectionEl, seaTac.total);
-seattleCenter.generate(seattleCenter.minCust, seattleCenter.maxCust, seattleCenter.average, seattleCenter.array, seattleCenter.sectionEl, seattleCenter.total);
-capHill.generate(capHill.minCust, capHill.maxCust, capHill.average, capHill.array, capHill.sectionEl, capHill.total);
-alki.generate(alki.minCust, alki.maxCust, alki.average, alki.array, alki.sectionEl, alki.total);
+function makeTable(stores, times){
+  for(var j = 0; j < times.length; j++){
+    createElement('th', 'class', 'time', times[j], head);
+  }
+  for (var i = 0; i < stores.length; i++){
+    var currentStore = stores[i];
+    var rowEl = createElement('tr', 'id', currentStore.tag, currentStore.name, tableEl);
+  }
+}
+makeTable(stores, times);
+document.body.appendChild(tableEl);
+
+  //  firstAndPike.generate(currentStore.minCust, currentStore.maxCust, currentStore.average, currentStore.array, document.getElementById(currentStore.tag), currentStore.tag, currentStore.total);
+
+firstAndPike.generate(firstAndPike.minCust, firstAndPike.maxCust, firstAndPike.average, firstAndPike.array, document.getElementById(firstAndPike.tag), firstAndPike.total);
+seaTac.generate(seaTac.minCust, seaTac.maxCust, seaTac.average, seaTac.array, document.getElementById(seaTac.tag), seaTac.total);
+seattleCenter.generate(seattleCenter.minCust, seattleCenter.maxCust, seattleCenter.average, seattleCenter.array, document.getElementById(seattleCenter.tag), seattleCenter.total);
+capHill.generate(capHill.minCust, capHill.maxCust, capHill.average, capHill.array, document.getElementById(capHill.tag), capHill.total);
+alki.generate(alki.minCust, alki.maxCust, alki.average, alki.array, document.getElementById(alki.tag), alki.total);
+/*
+seaTac.generate(seaTac.minCust, seaTac.maxCust, seaTac.average, seaTac.array, seaTac.total);
+seattleCenter.generate(seattleCenter.minCust, seattleCenter.maxCust, seattleCenter.average, seattleCenter.array, seattleCenter.total);
+capHill.generate(capHill.minCust, capHill.maxCust, capHill.average, capHill.array, capHill.total);
+alki.generate(alki.minCust, alki.maxCust, alki.average, alki.array, alki.total);
+console.log(stores);
+*/
